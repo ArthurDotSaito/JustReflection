0f2cf15a97433e7301e981ae6a766abd585ebcda
Arthur Saito <arthur.saito@nuria.com.br>
2024-09-04 21:39:57 -0300

    moving from SQLServer to MySQL

---
+++ JustReflection.EF/CreateTable.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace JustReflection.EF;
+
+public class CreateTable
+{
+    public static void CreateSQLTable(Type type, string tableName = null)
+    {
+        var connectionString = "server=localhost;port=3307;database=reflection;user=root;password=root";
+        try
+        {
+            using (var dbConnection = new MySqlConnection(connectionString))
+            {
+                var properties = type.GetProperties();
+                var queryBuilder = new StringBuilder();
+                queryBuilder.Append($"CREATE TABLE `{tableName}` (");
+                var columns = new List<string>();
+
+                foreach (var prop in properties)
+                {
+                    if (prop.Name.ToLower() == "id" && prop.PropertyType.Name == "Int32")
+                        columns.Add("Id INT PRIMARY KEY AUTO_INCREMENT");
+            
+                    if(prop.PropertyType.Name == "String")
+                        columns.Add($"{prop.Name} VARCHAR(255)");
+                }
+        
+                string columnsString = string.Join(", ", columns);
+
+                queryBuilder.Append(columnsString);
+                queryBuilder.Append(")");
+        
+                var command = new MySqlCommand(queryBuilder.ToString(), dbConnection);
+                dbConnection.Open();
+                command.ExecuteNonQuery();
+        
+                dbConnection.Close();
+                Console.WriteLine($"{tableName} table created successfully!");
+            }
+        }catch(Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
--- JustReflection.EF/Program.cs
+++ JustReflection.EF/Program.cs
@@ -1,45 +1,21 @@
-﻿using System;
-using System.Data.SqlClient;
-using System.Reflection;
-using System.Text;
-
+﻿
 namespace JustReflection.EF;
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello World!");
+        CreateTable.CreateSQLTable(typeof(Person), "People");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 
-    static void CreateSQLTable(Type type, string tableName = null)
+    public class Person()
     {
-        var dbConnection = new SqlConnection("server=localhost;database=reflection;integrated security=true;");
-
-        var properties = type.GetProperties();
-        var queryBuilder = new StringBuilder();
-        queryBuilder.Append($"CREATE TABLE {tableName}");
-        var columns = new List<string>();
-
-        foreach (var prop in properties)
-        {
-            if (prop.Name.ToLower() == "id" && prop.PropertyType.Name == "Int32")
-                columns.Add("Id INT PRIMARY KEY IDENTITY(1,1)");
-            
-            if(prop.PropertyType.Name == "String")
-                columns.Add($"{prop.Name} nvarchar(max)");
-        }
-        
-        string columnsString = string.Join(", ", columns);
-
-        queryBuilder.Append(columnsString);
-        queryBuilder.Append(")");
-        
-        var command = new SqlCommand(queryBuilder.ToString(), dbConnection);
-        dbConnection.Open();
-        command.ExecuteNonQuery();
-        
-        dbConnection.Close();
-        Console.WriteLine($"{tableName} table created successfully!");
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string ZipCode { get; set; }
     }
+    
     
 }
